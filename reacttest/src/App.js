43fec99acfc4695550f0a9dafec6ed40ef45cd3a
mifontelera1 <mifontelera1@gmail.com>
2020-03-30 14:34:01 +0800

    downloaded react

---
+++ reacttest/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>Hello This is a test</p>
+    </div>
+  );
+}
+
+export default App;
